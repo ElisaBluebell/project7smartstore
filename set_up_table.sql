e04775ec08d234ef5c2725c55e65956bab6063fc
ElisaBluebell <selfishracer@hotmail.com>
2023-01-16 15:23:47 +0900

    230116 ElisaBluebell

--- set_up_table.sql
+++ set_up_table.sql
@@ -1,4 +1,5 @@
 -- 스키마 제작
-CREATE SCHEMA `project7smartstore`;
+-- CREATE SCHEMA `project7smartstore`;
 -- 회원 가입을 통해 받는 유저 정보 테이블
-CREATE TABLE `project7smartstore`.`user_info` (`idx` INT NOT NULL, `user_id` VARCHAR(16) NOT NULL, `user_pw` VARCHAR(16) NOT NULL, `user_name` VARCHAR(16) NOT NULL, `user_tel` INT NOT NULL, `temp` VARCHAR(16) NOT NULL, `user_type` INT NOT NULL);
+CREATE TABLE `project7smartstore`.`user_info` (`idx` INT NOT NULL AUTO_INCREMENT, `user_id` VARCHAR(16) NOT NULL, `user_pw` VARCHAR(16) NOT NULL, `user_name` VARCHAR(16) NOT NULL, `user_tel` INT NOT NULL, `temp` VARCHAR(16) NOT NULL, `user_type` INT NOT NULL, PRIMARY KEY(idx))
+-- 상품 정보 테이블
