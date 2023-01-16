2ab44539356c77df10e2cadbca6f9b378b62e24f
ElisaBluebell <selfishracer@hotmail.com>
2023-01-16 19:33:46 +0900

    230116 ElisaBluebell

---
+++ inventory_management.py
@@ -0,0 +1,29 @@
+import
+
+class InventoryManagement:
+
+    def __init__(self):
+        super().__init__()
+
+    def set_label(self):
+        pass
+
+    def set_line(self):
+        pass
+
+    def set_btn(self):
+        pass
+
+    def set_combo(self):
+        pass
+
+    def set_table(self):
+        pass
+
+    def set_ui(self):
+        self.set_label()
+        self.set_line()
+        self.set_btn()
+        self.set_combo()
+        self.set_table()
+
