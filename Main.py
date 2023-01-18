38c87232ccd15b7f1a1c19d4d2b7b60b4bf92c0a
ElisaBluebell <selfishracer@hotmail.com>
2023-01-18 12:27:43 +0900

    230118 ElisaBluebell

--- Main.py
+++ Main.py
@@ -271,7 +271,7 @@ class MainPage(QWidget, MainUIset):
         self.bom_ingredient_table.setColumnWidth(2, 240)
 
         self.get_bom_table_db()
-        self.bom_select_menu.currentTextChanged.connect(self.set_bom_table_logic)
+        self.bom_select_menu.currentTextChanged.connect(self.menu_changed)
 
     def get_bom_table_db(self):
 
@@ -300,6 +300,10 @@ class MainPage(QWidget, MainUIset):
         self.bom_ingredient_table.setItem(row, column, QTableWidgetItem(self.table_data[i][j]))
         self.bom_ingredient_table.item(row, column).setToolTip(self.table_data[i][j])
 
+    def menu_changed(self):
+        self.set_bom_table_logic()
+        self.set_bom_available()
+
     def set_bom_table_logic(self):
         if self.bom_select_menu.currentText() == '전체':
             self.bom_table_default_data()
@@ -320,6 +324,31 @@ class MainPage(QWidget, MainUIset):
                     for j in range(len(self.table_data[i])):
                         self.set_bom_table_data_tooltip(bom_table_row - 1, bom_table_column, i, j)
                         bom_table_column += 1
+
+    def set_bom_available(self):
+        if self.bom_select_menu.currentText() == '전체':
+            self.bom_available.setText('')
+
+        else:
+            conn = pymysql.connect(host='10.10.21.106', port=3306, user='root', password='1q2w3e4r',
+                                   db='project7smartstore')
+            c = conn.cursor()
+
+            # 콤보박스에서 선택된 메뉴의 재료들 중 가장 작은 재료재고/재료소모량 값을 가져옴
+            c.execute(f'''SELECT min(b.inventory_quantity DIV a.material_quantity) AS produce_available 
+            FROM bill_of_material AS a 
+            INNER JOIN material_management AS b 
+            ON a.material_name=b.material_name 
+            WHERE a.product_name="{self.bom_select_menu.currentText()}"''')
+
+            producible = c.fetchall()[0][0]
+
+            if producible < 10:
+                self.bom_available.setStyleSheet("Color: red")
+            else:
+                self.bom_available.setStyleSheet("Color: black")
+            print(producible)
+            self.bom_available.setText(f'{str(producible)}개 제작 가능')
 
     def buy_ingredient_window(self):
         self.ingredient_window.show()
